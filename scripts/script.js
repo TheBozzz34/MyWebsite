6b30644bab0839976451495dd6ed98ed5751c071
Ethan James <lskywalker416@gmail.com>
2022-02-15 09:26:47 -0700

    Switch to Webstorm Formatting
    Signed-off-by: Ethan James <lskywalker416@gmail.com>

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+let visited = Cookies.get('visited');
+    if (visited == "yes") {
+        //alert("Welcome again");
+        console.log(visited);
+    } else {
+        window.location.replace("https://fourleafclover.company/quaternion-inference.html");
+  }
+    function setLocation0() {
+      window.location.replace("https://github.com/TheBozzz34/");
+    }
+    function setLocation1() {
+      window.location.replace("https://twitter.com/gonk_droid101");
+    }
+    function setLocation2() {
+      window.location.replace("https://instagram.com/k._eturah");
+    }

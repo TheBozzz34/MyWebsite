a6ba98971d4e2adb60b6263b11f7591358b6c1a0
Ethan James <lskywalker416@gmail.com>
2022-11-16 19:08:18 -0700

    Create time.js

---
+++ scripts/time.js
@@ -0,0 +1,8 @@
+el=document.getElementById("time");
+
+setInterval(function setTime(){ 
+    let date1 = new Date;
+    let date2 = Date.parse("1668650832");
+    let timeFrom = Math.abs(date1 - date2) / 36e5;
+    el.innerHtml(timeFrom);    
+}, 1000);

2d91df7331ac57e3fe8ac0f3f5e58cca688508ab
Ethan James <lskywalker416@gmail.com>
2022-11-20 07:33:38 -0700

    Update time.js

--- scripts/time.js
+++ scripts/time.js
@@ -4,5 +4,5 @@ setInterval(function setTime(){
     let date1 = new Date;
     let date2 = Date.parse("1668650832");
     let timeFrom = Math.abs(date1 - date2) / 36e5;
-    el.innerHtml(timeFrom);    
+    el.textContent = timeFrom;    
 }, 1000);

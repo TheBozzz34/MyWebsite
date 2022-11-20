c4ccede10b1cf2e8844819cf88ded50d41afb5b7
Ethan James <lskywalker416@gmail.com>
2022-11-20 07:52:41 -0700

    Update time.js

--- scripts/time.js
+++ scripts/time.js
@@ -1,5 +1,20 @@
 el=document.getElementById("time");
 
+function timeDiff( tstart, tend ) {
+  var diff = Math.floor((tend - tstart) / 1000), units = [
+    { d: 60, l: "seconds" },
+    { d: 60, l: "minutes" },
+    { d: 24, l: "hours" },
+    { d: 7, l: "days" }
+  ];
+
+  var s = '';
+  for (var i = 0; i < units.length; ++i) {
+    s = (diff % units[i].d) + " " + units[i].l + " " + s;
+    diff = Math.floor(diff / units[i].d);
+  }
+  return s;
+}
 
 setInterval(function setTime(){ 
     let date1 = Date.now();
@@ -11,6 +26,7 @@ setInterval(function setTime(){
     var diffSeconds = differenceDate.getUTCSeconds();
     
     var readableDifference = diffHours + ':' + diffMinutes + ':' + diffSeconds;
+    var test = timeDiff( date2, date1);
     
-    el.textContent = readableDifference;    
+    el.textContent = test;    
 }, 1000);

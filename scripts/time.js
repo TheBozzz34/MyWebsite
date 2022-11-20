27551476ce0a45f9ec907b25310b28b715388663
Ethan James <lskywalker416@gmail.com>
2022-11-20 07:44:40 -0700

    Update time.js

--- scripts/time.js
+++ scripts/time.js
@@ -1,8 +1,16 @@
 el=document.getElementById("time");
 
+
 setInterval(function setTime(){ 
-    let date1 = new Date;
-    let date2 = Date.parse("1668650832");
-    let timeFrom = Math.abs(date1 - date2) / 36e5;
-    el.textContent = timeFrom;    
+    let date1 = Math.floor(new Date('2022.11.20').getTime() / 1000);
+    let date2 = Math.floor(new Date('2022.11.17').getTime() / 1000);
+    var timeDifference = date1 - date2;
+    var differenceDate = new Date(timeDifference * 1000);
+    var diffHours = differenceDate.getUTCHours();
+    var diffMinutes = differenceDate.getUTCMinutes();
+    var diffSeconds = differenceDate.getUTCSeconds();
+    
+    var readableDifference = diffHours + ':' + diffMinutes + ':' + diffSeconds;
+    
+    el.textContent = readableDifference;    
 }, 1000);
